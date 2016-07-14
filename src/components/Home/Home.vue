b02dabade5361ecd58af563c7055f54f0397d579
Albin Martinsson <albinmartinsson91@gmail.com>
2016-07-14 17:56:22 +0200

    Restyled landing and began adding queries for large screens.

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -1,18 +1,32 @@
 <template>
     <div class="landing--container flex align-center align-middle" v-if="show">
       <div class="row">
-          <div class="col-lg-7 col-md-7 bg landing--intro text-left flex align-middle">
+          <div class="col-xs-12 col-md-10 col-lg-10 bg landing--intro margin-auto text-center flex align-middle">
             <h1 v-if="data.title">{{ data.title.rendered }}</h1>
           </div>
-          <div class="col-lg-5 col-md-5 bg landing--carousel flex align-middle" v-on:click="goToProject">
+          <div class="col-xs-12 bg landing--carousel flex align-middle" v-on:click="goToProject">
             <div class="carousel--item row text-left">
-              <div class="item--left col-xs-5 col-md-12">
-                <div class="item--image" v-bind:style="{'background-image': 'url(' + featuredRandom.custom_field.project_featured_image + ')' }"></div>
-                <div class="item--shadow"></div>
+              <div class="item--left col-xs-6">
+              
+                  <img class="item--image" src="{{featuredRandom.custom_field.project_feature_image}}">
               </div>
-              <div class="item--right col-xs-7 col-md-12 align-middle align-center">
-                <span>Utvalt projekt</span>
+              <div class="item--right col-xs-6 align-left align-center">
+                <span>Utvalt case</span>
                 <h2 v-if="featured">{{ featuredRandom.title.rendered }}</h2>
+                <div class="project--arrow flex align-middle align-center">
+                  <?xml version="1.0" encoding="UTF-8" standalone="no"?>
+                    <svg width="25px" height="25px" viewBox="0 0 36 36" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                        <defs></defs>
+                        <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
+                            <g id="Tjänster-&amp;-Projekt-Copy-6" transform="translate(-1261.000000, -3125.000000)" stroke="#FFFFFF" stroke-width="5.59999957">
+                                <g id="Group-12" transform="translate(1264.000000, 3128.000000)">
+                                    <path d="M0.566037736,14.7272727 L28.0997842,14.7272727" id="Path-36"></path>
+                                    <polyline id="Path-37" points="14.1509434 0 29.5989795 14.8862893 14.1509434 29.7725787"></polyline>
+                                </g>
+                            </g>
+                        </g>
+                    </svg>
+                  </div>
               </div>
             </div>
           </div>
