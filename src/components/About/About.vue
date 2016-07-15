60928578999791550106ddca656e9e6582196765
Albin Martinsson <albinmartinsson91@gmail.com>
2016-07-15 14:34:52 +0200

    Began fixing the about page.

--- src/components/About/About.vue
+++ src/components/About/About.vue
@@ -3,16 +3,48 @@
       <h1>{{data.title.rendered}}</h1>
       <!--<p>{{{data.content.rendered}}}</p>-->
   </div>
-   <div class="row about--container" v-if="show">
-        <div class="about--contact text-left col-xs-12 flex align-left align-center" v-for="sub in data.sub_pages" v-bind:style="{'background-image': 'url(' + sub.custom_fields.work_image + ')' }">
-          <div class="contact--info">
-            <h2>{{sub.custom_fields.work_title}}</h2>
+
+      <div class="about--container" v-if="show">
+      <div class="row about--employee" v-for="sub in data.sub_pages" v-bind:style="{background: sub.custom_fields.work_color }">
+          <div class="col-xs-12 col-md-8 text-left about--description">
+            <h1>{{sub.post_title}}</h1>
+             <p>{{sub.custom_fields.work_title}}</p>
             <p>{{sub.custom_fields.work_description}}</p>
-            <h3><a href="">{{sub.custom_fields.work_phone}}</a></h3>
-            <h3><a href="">{{sub.custom_fields.work_email}}</a></h3>
+            <a href="">{{sub.custom_fields.work_phone}}
+              <span><?xml version="1.0" encoding="UTF-8" standalone="no"?>
+                    <svg width="25px" height="25px" viewBox="0 0 36 36" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                        <defs></defs>
+                        <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
+                            <g id="Tjänster-&amp;-Projekt-Copy-6" transform="translate(-1261.000000, -3125.000000)" stroke="#FFFFFF" stroke-width="5.59999957">
+                                <g id="Group-12" transform="translate(1264.000000, 3128.000000)">
+                                    <path d="M0.566037736,14.7272727 L28.0997842,14.7272727" id="Path-36"></path>
+                                    <polyline id="Path-37" points="14.1509434 0 29.5989795 14.8862893 14.1509434 29.7725787"></polyline>
+                                </g>
+                            </g>
+                        </g>
+                    </svg>
+                </span>
+            </a>
+            <a href="">{{sub.custom_fields.work_email}}
+               <span>
+               <?xml version="1.0" encoding="UTF-8" standalone="no"?>
+                    <svg width="25px" height="25px" viewBox="0 0 36 36" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                        <defs></defs>
+                        <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
+                            <g id="Tjänster-&amp;-Projekt-Copy-6" transform="translate(-1261.000000, -3125.000000)" stroke="#FFFFFF" stroke-width="5.59999957">
+                                <g id="Group-12" transform="translate(1264.000000, 3128.000000)">
+                                    <path d="M0.566037736,14.7272727 L28.0997842,14.7272727" id="Path-36"></path>
+                                    <polyline id="Path-37" points="14.1509434 0 29.5989795 14.8862893 14.1509434 29.7725787"></polyline>
+                                </g>
+                            </g>
+                        </g>
+                    </svg>
+                </span>
+            </a>
           </div>
-        </div>
+           <img v-bind:src="sub.custom_fields.work_image"/>
       </div>
+    </div>
 </template>
 
 <script>
