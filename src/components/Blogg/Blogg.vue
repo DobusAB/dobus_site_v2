7d5f5461c71e3d67c8a0490315034b564f4821f9
johandobus <johan@dobus.se>
2016-07-27 15:33:20 +0200

    doing some stuff..

--- src/components/Blogg/Blogg.vue
+++ src/components/Blogg/Blogg.vue
@@ -1,13 +1,9 @@
 <template>
-<div class="landing--container" v-if="show">
-  <div class="row">
-    <div class="col-xs-12 landing--intro margin-auto text-center flex align-middle">
-      <h1 class="text--tilted" v-bind:class="{'fadeIn' : this.$root.global.loading}">{{{ data.title.rendered }}}</h1>
-    </div>
-  </div>
-</div>
+   <div class="page--intro flex align-left align-middle" v-if="show">
+      <h1 class="text--tilted">{{{data.title.rendered}}}</h1>
+   </div>
  <div class="row">
- <div class="blog--wrapper col-xs-12 col-lg-4" v-bind:style="{background: post.custom_field.post_color }" v-for="post in posts" v-link="{name: 'post_by_id', params: {id: post.id}}">
+ <div class="blog--wrapper col-xs-12 col-lg-4" v-bind:style="{background: post.custom_field.post_color }" v-for="post in items" v-link="{name: 'post_by_id', params: {id: post.id}}">
     <div class="blog--image">
        <div class="masked--image small">
           <div class="masked--image_inner" v-bind:style="{'background-image': 'url(' + post.custom_field.post_image + ')' }"></div>
@@ -17,10 +13,10 @@
       <h4>{{post.title.rendered}}</h1>
       <p>{{{post.excerpt.rendered}}}</p>
        <div class="author--wrapper flow--horizontal flex">
-        <div class="author--image"  v-bind:style="{'background-image': 'url(' + data[0].custom_field.author_image + ')' }"></div>
+       <!--  <div class="author--image"  v-bind:style="{'background-image': 'url(' + data[0].custom_field.author_image + ')' }"></div>-->
         <div class="author--info">
-          <h5>{{{data[0].custom_field.author_title}}}</h5>
-          <h4>{{{data[0].custom_field.author_name}}}</h4>
+         <!-- <h5>{{{data[0].custom_field.author_title}}}</h5> -->
+         <!-- <h4>{{{data[0].custom_field.author_name}}}</h4> -->
         </div>
       </div>
     </div>
@@ -28,7 +24,6 @@
 </div>
 <router-view></router-view>
 </template>
-
 <script>
 import Init from '../Partials/Init'
 import Loading from '../Partials/Loading'
@@ -42,11 +37,29 @@ export default {
       msg: 'Hello World',
       show: false,
       data: [],
-      posts: [],
+      items: [],
       yoast: {
         description: '',
         title: '',
         keywords: ''
+      },
+      featured: [],
+      featuredRandom: [],
+      hidden: false
+    }
+  },
+  head: {
+    title: function () {
+      return {
+        inner: this.yoast.title
+      }
+    },
+    meta: function () {
+      return {
+        name: {
+          description: this.yoast.description,
+          keywords: this.yoast.keywords
+        }
       }
     }
   },
@@ -57,18 +70,26 @@ export default {
     getPageData: function (transition) {
       this.$http.get(Init.globalUrl() + 'index.php/wp-json/wp/v2/pages/573').then((response) => {
         this.data = response.data
-        this.getPosts(transition)
+        this.yoast.description = response.data.yoast_meta.yoast_wpseo_metadesc
+        this.yoast.title = response.data.yoast_meta.yoast_wpseo_title
+        this.yoast.keywords = response.data.yoast_meta.yoast_wpseo_focuskw
+        this.getFeatured(transition)
       },
       (response) => {})
     },
-    getPosts: function (transition) {
+    getFeatured: function (transition) {
       this.$http.get(Init.globalUrl() + 'index.php/wp-json/wp/v2/posts').then((response) => {
-        this.posts = response.data
-        this.show = true
         this.$root.global.loading = false
+        // this.featuredRandom = this.featured[Math.floor(Math.random() * this.featured.length)]
+        this.show = true
+        this.items = response.data
         transition.next(transition)
       },
-			(response) => {})
+      (response) => {})
+    },
+    goToProject: function () {
+      console.log(this.featuredRandom)
+      this.$router.go({name: 'project_by_name', params: {name: this.featuredRandom.slug}})
     }
   },
   route: {
