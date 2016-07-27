65f34adc6035c171ca943f4c54351b629e5cfeb5
johandobus <johan@dobus.se>
2016-07-27 10:56:01 +0200

    fixed node problems

--- src/components/Blogg/Blogg.vue
+++ src/components/Blogg/Blogg.vue
@@ -35,21 +35,6 @@ export default {
       }
     }
   },
-  head: {
-    title: function () {
-      return {
-        inner: this.yoast.title
-      }
-    },
-    meta: function () {
-      return {
-        name: {
-          description: this.yoast.description,
-          keywords: this.yoast.keywords
-        }
-      }
-    }
-  },
   components: {
     loading: Loading
   },
